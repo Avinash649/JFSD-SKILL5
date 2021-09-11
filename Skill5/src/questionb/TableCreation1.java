430a492852b93d42ca50388feb1d48e863bbc894
A.Avinash <65214275+Avinash649@users.noreply.github.com>
2021-09-11 13:57:52 +0530

    Update TableCreation1.java

--- Skill5/src/questionb/TableCreation1.java
+++ Skill5/src/questionb/TableCreation1.java
@@ -8,7 +8,7 @@ import java.sql.Statement;
 public class TableCreation1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		try {//190030061
+		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 			Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","jfsd","jfsd123");
 			Statement st = con.createStatement();
